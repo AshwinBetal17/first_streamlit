2da994aa9fd75337aa228d0e1c4c30144c57f51c
AshwinBetal17 <112087300+AshwinBetal17@users.noreply.github.com>
2023-05-18 13:37:02 +0530

    Update streamlit_app.py
    Added breakfast menu header and 3 items.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit
 
-streamlit.title('My Parents are happy Now')
+streamlit.title('My Parents new healthy dinner')
 
-streamlit.header('My Name is Ashwin Betal')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
