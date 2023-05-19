923fb5e8af666e737d9389cc0b348ef5551e93e3
AshwinBetal17 <112087300+AshwinBetal17@users.noreply.github.com>
2023-05-19 12:59:32 +0530

    Update streamlit_app.py
    Added jackfruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,7 @@ try:
   if not fruit_choice:
       streamlit.error("Please select a fruit to get information")
   else:
-      back_from_function = get_fruityvice_data('kiwi')
+      back_from_function = get_fruityvice_data('jackfruit')
       streamlit.dataframe(back_from_function)
 
 except URLError as e:
