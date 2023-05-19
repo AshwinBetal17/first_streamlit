4bc8921890fc7d0a6d159dfe832dca0fc3370ecf
AshwinBetal17 <112087300+AshwinBetal17@users.noreply.github.com>
2023-05-19 12:04:21 +0530

    Update streamlit_app.py
    added comments

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,15 +26,17 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 try:
-    fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-    if not fruit_choice:
-        streamlit.write('The user entered ', fruit_choice)
-    else:
-        # write your own comment -what does the next line do? 
-        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-        # write your own comment - what does this do?
-        streamlit.dataframe(fruityvice_normalized)
+  fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+  if not fruit_choice:
+      streamlit.write('The user entered ', fruit_choice)
+  else:
+      # write your own comment -what does the next line do? 
+      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+      # write your own comment - what does this do?
+      streamlit.dataframe(fruityvice_normalized)
 
+except URLError as e:
+    streamlit.error()
 # import requests
 # fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 # streamlit.text(fruityvice_response.json())
@@ -44,8 +46,7 @@ try:
 # # write your own comment - what does this do?
 # streamlit.dataframe(fruityvice_normalized)
 
-except URLError as e:
-    streamlit.error()
+
 
 
 
